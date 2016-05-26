e14231de25a01bf048759f785a9f05cee8b6e247
Joe <joe@animustechnologies.com>
2016-05-26 18:12:17 +0100

    yay

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
 'use strict';
 
 var resin = require('resin-sdk');
-var exphbs  = require('express-handlebars');
+var handlebars = require('express-handlebars');
 var express = require('express');
 var app = express();
 
-var urls = [];
-
-var hbs = exphbs.create({
-    helpers: {
-        url: function () {
-        	console.log(urls.length);
-        	return urls.shift();
-        }
-    }
-});
-
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+enableDevices();
+
 app.get('/', function (req, res, next) {
+	getDevices(res)
+});
+app.listen(3000);
+
+function getDevices(res) {
+  resin.auth.authenticate({email: process.env.EMAIL, password: process.env.PASSWORD}, function(error, token) {
+	    if (error) throw error;
+	    resin.models.device.getAllByApplication(process.env.APP_NAME, function(error, devices) {
+		    if (error) throw error;
+		    res.render('home', { 'devices': devices });
+		});
+	});
+}
+
+function enableDevices() {
 	resin.auth.authenticate({email: process.env.EMAIL, password: process.env.PASSWORD}, function(error, token) {
 	    if (error) throw error;
 	    resin.models.device.getAllByApplication(process.env.APP_NAME, function(error, devices) {
 		    if (error) throw error;
-		    var iter = 0;
 		    devices.forEach(function(device) {
 				resin.models.device.enableDeviceUrl(device.uuid, function(error) {
 				    if (error) throw error;
-				    resin.models.device.getDeviceUrl(device.uuid, function(error, url) {
-					    if (error) throw error;
-					    urls.push(url + "/html/");
-					    iter++;
-					    if(iter == devices.length)
-					    {
-					    	res.render('home');
-					    }
-					});
 				});
 			});
 		});
 	});
-});
-
-app.listen(3000);
-
-
-
+}
